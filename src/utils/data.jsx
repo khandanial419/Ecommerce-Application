91faa57e8c1b69666438559cba23ee5144c71f8b
Daniyal Anwar khan <77050423+khandanial419@users.noreply.github.com>
2024-05-22 13:24:08 +0500

    update

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -247,34 +247,48 @@ export const Menproducts = [
 
 export const CarisoulData = [
   {
-   
-    title:"more luxury and trending",
-    name: `Wood Cup Of Tea`,
-    img: "https://images.unsplash.com/photo-1618886614638-80e3c103d31a?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+    caption: `San Francis`,
   },
   {
-    title:"more luxury and trending",
-    name: `Wood Cup Of Tea`,
-    img:  "https://images.unsplash.com/photo-1554412933-514a83d2f3c8?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    image:
+      "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+    caption: "Scotland",
   },
   {
-    title:"more luxury and trending",
-    name: `Wood Cup Of Tea`,
-    img:  "https://images.unsplash.com/photo-1554412933-514a83d2f3c8?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    image:
+      "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+    caption: "Darjeeling",
   },
   {
-    title:"more luxury and trending",
-    name: `Wood Cup Of Tea`,
-    img:  "https://images.unsplash.com/photo-1554412933-514a83d2f3c8?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
+    caption: "San Francisco",
   },
   {
-    title:"more luxury and trending",
-    name: `Wood Cup Of Tea`,
-    img:  "https://images.unsplash.com/photo-1554412933-514a83d2f3c8?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    image:
+      "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
+    caption: "Scotland",
+  },
+  {
+    image:
+      "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
+    caption: "Darjeeling",
+  },
+  {
+    image:
+      "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
+    caption: "San Francisco",
+  },
+  {
+    image:
+      "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
+    caption: "Scotland",
+  },
+  {
+    image:
+      "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
+    caption: "Darjeeling",
   },
 ];
